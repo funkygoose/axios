33cae04e92ead1ab131f0e985af1594cc873db09
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-04-19 23:37:24 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/"></Route>
-          <Route path=":id"></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path=":id" element={<Posts />}></Route>
         </Routes>
       </div>  
     </Router>
