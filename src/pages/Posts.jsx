67cc98f59e5ae7a7a4a4cd19d602f3e18a98a13c
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-04-20 09:01:28 -0700

    initial commit

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 
 function Posts() {
     const { id } = useParams();
-    console.log(id);
+    useEffect(() => {
+        const data = fetch("https://jsonplaceholder.typicode.com/posts/1")
+        console.log(data)
+    }, []);
   return (
     <div>{id}</div>
   )
