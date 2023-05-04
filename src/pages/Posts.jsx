969fada3342e03e4e4239c8c5eadba551d79c6bf
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-05-04 09:43:09 -0700

    initial commit

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,46 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Posts = () => {
-    const { id } = useParams();
-    const [posts, setPosts] = useState([]);
-    
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        async function fetchPosts() {
-            const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-            setPosts(data);
-        }
-        fetchPosts();
-    }, []);
-    
+  useEffect(() => {
+    async function fetchPosts() {
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+      );
+      setPosts(data);
+    }
+    fetchPosts();
+  }, []);
+
   return (
     <>
-  <div className="post__search">
-    <button>← Back</button>
-    <div className="post__search--container">
-      <label className="post__search--label">Search by Id</label>
-      <input
-        type="number"
-      />
-      <button>Enter</button>
-    </div>
-  </div>
-  <div className="post">
-    <div className="post__title">post.title</div>
-    <p className="post__body">post.body</p>
-  </div>
-  <div className="post">
-    <div className="post__title">
-      <div className="post__title--skeleton"></div>
-    </div>
-    <div className="post__body">
-      <p className="post__body--skeleton"></p>
-    </div>
-  </div>
-</>
-  )
-}
+      <div className="post__search">
+        <button>← Back</button>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+          <input type="number" />
+          <button>Enter</button>
+        </div>
+      </div>
+      {posts.map((post) => (
+        <div className="post">
+          <div className="post__title">{post.title}</div>
+          <p className="post__body">{post.body}</p>
+        </div>
+      ))}
 
-export default Posts
+      <div className="post">
+        <div className="post__title">
+          <div className="post__title--skeleton"></div>
+        </div>
+        <div className="post__body">
+          <p className="post__body--skeleton"></p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Posts;
