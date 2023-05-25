445b647b588f671ffb973f6f5418e49b4ea4a076
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-05-25 10:11:28 -0700

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState} from "react";
+import 
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
 
-export default Home
+    const [users, setUsers] 
+  "https://jsonplaceholder.typicode.com/users"
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          <div className="user">
+            <div className="user-card">
+              <div className="user-card__container">
+                <h3>{user.name}</h3>
+                <p>
+                  <b>Email:</b> {user.email}
+                </p>
+                <p>
+                  <b>Phone:</b> {user.phone}
+                </p>
+                <p>
+                  <b>Website:</b>
+                  {user.website}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
